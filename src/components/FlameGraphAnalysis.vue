f9cf1a0759d584bfe338dd0c50f1620f2834dcb0
liuzelong <742743697@qq.com>
2024-08-11 20:28:35 +0800

    Echarts报表展示

--- src/components/FlameGraphAnalysis.vue
+++ src/components/FlameGraphAnalysis.vue
@@ -1,12 +1,24 @@
 <template>
   <div class="flame-graph-analysis">
-    asd
+    <iframe
+      :src="flameGraphUrl"
+      allowfullscreen
+      frameborder="0"
+      height="100%"
+      width="100%"
+    >
+    </iframe>
   </div>
 </template>
 
 <script>
 export default {
-  name: "FlameGraphAnalysis"
+  name: "FlameGraphAnalysis",
+  data() {
+    return {
+      flameGraphUrl: "http://127.0.0.1:9999/flame-graph.html"
+    }
+  }
 }
 </script>
 
